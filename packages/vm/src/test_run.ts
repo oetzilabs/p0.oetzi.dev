168ae5b2a0d19399f5cdd8b52116a15774e643f9
Özgür Isbert <oezguerisbert@gmail.com>
2025-03-18 15:48:32 +0100

    chores

--- packages/vm/src/test_run.ts
+++ packages/vm/src/test_run.ts
@@ -3,8 +3,9 @@ import { FirecrackerService, FirecrackerLive } from ".";
 import { Effect } from "effect";
 
 const program = Effect.gen(function* (_) {
-  const fcService = yield* _(FirecrackerService);
-  return yield* fcService.prepare();
+  const firecracker = yield* _(FirecrackerService);
+  yield* firecracker.prepare();
+  return;
 }).pipe(Effect.provide(FirecrackerLive), Effect.provide(BunContext.layer), Effect.provide(BunFileSystem.layer));
 
 BunRuntime.runMain(Effect.scoped(program));
