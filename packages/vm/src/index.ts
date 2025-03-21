13b9e01a18574e3677c1d826eaf165f461ea3295
Özgür Isbert <oezguerisbert@gmail.com>
2025-03-21 23:25:48 +0100

    wip: jailer

--- packages/vm/src/index.ts
+++ packages/vm/src/index.ts
@@ -414,7 +414,7 @@ export class FirecrackerService extends Effect.Service<FirecrackerService>()("@p
 
     const settingUpFirecrackerVM = (config: VmConfig) =>
       Effect.gen(function* (_) {
-        const vmSocketPath = socketGen(config.vmId, 2);
+        const vmSocketPath = socketGen(config.vmId, 1);
         yield* logger.info("createFirecrackerVM", "vmSocketPath", vmSocketPath);
         yield* logger.info("createFirecrackerVM", "FIRECRACKER_BINARY", FIRECRACKER_BINARY);
         const firecrackerCommand = Command.make(FIRECRACKER_BINARY, "--api-sock", vmSocketPath).pipe(env);
